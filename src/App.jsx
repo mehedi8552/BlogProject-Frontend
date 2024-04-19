39b4243c8e877119f9711a0cf0e9bd83a41a4db4
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2024-04-20 00:32:05 +0800

    test

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import NavBar from "./Layout/NavBar"
 import BloggerSignUp from "./Component/BloggerSignUp"
 import BloggerLogin from "./Component/BloggerLogin"
-import Layout from '../src/Layout/Layout'
+//import Layout from '../src/Layout/Layout'
 import HomePage from "./Page/HomePage"
 import ContactPage from "./Page/ContactPage"
 import AbouPage from "./Page/AbouPage"
@@ -14,10 +14,15 @@ function App() {
     <BrowserRouter>
     <Routes>
     <Route path="/" element={<HomePage/>}/>
+
     <Route path="/contacts" element={<ContactPage/>}/>
+
     <Route path="/About-us" element={<AbouPage/>}/>
+
     <Route path="/ccc" element={<ManageBlogPage/>}/>
+
     <Route path="/hello" element={<SignUpPage/>}/>
+    
     <Route path="/hello" element={<ContactPage/>}/>
     </Routes>
     </BrowserRouter>
